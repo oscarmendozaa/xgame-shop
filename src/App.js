c5ca6abd54fecd0cd3ea1a99628fb05cce9f904c
oscarmendozaa <mendoza.oscar0827@gmail.com>
2022-11-07 15:21:24 -0600

    Modificacion para pasar props al greeting

--- src/App.js
+++ src/App.js
@@ -2,16 +2,15 @@
 import './App.css';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import NavBar from './components/NavBar/NavBar';
-import Swal from 'sweetalert2'
-import {useEffect} from 'react'
+
 
 function App() {
   return (
     <div className="App">
       
-    <ItemListContainer/>  
-    <NavBar/>
     
+    <NavBar/>
+    <ItemListContainer name ="Bienvenido al portal"/>  
    
     </div>
   );
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -4,30 +4,29 @@ import Swal from 'sweetalert2'
 
 //CSS JS
 
-// const ItemListContainer = () => {
-//     console.log(props)
-//   return (
-//     <div className='message-greeting-container'>
-//         <h2 className='message'>{props.name}</h2>
-//     </div>
-//   )
-// }
+ const ItemListContainer = (props) => {
+   return (
+     <div className='message-greeting-container'>
+         <h2 className='message'>{props.name}</h2>
+     </div>
+   )
+ }
 
-// export default ItemListContainer
+ export default ItemListContainer
 
 //SWEETALERT
 
-const ItemListContainer = () => {
+// const ItemListContainer = () => {
 
-  Swal.fire({
-    title: 'Bienvenido al portal',
-    text: 'Xgame-shop',
-    imageUrl: 'https://img.unocero.com/2020/12/colaboracion-xbox-playstation-nintendo-seguridad-1-1-1024x512.jpg',
-    imageWidth: 400,
-    imageHeight: 200,
-    imageAlt: 'Custom image',
-  })
+//   Swal.fire({
+//     title: 'Bienvenido al portal',
+//     text: 'Xgame-shop',
+//     imageUrl: 'https://img.unocero.com/2020/12/colaboracion-xbox-playstation-nintendo-seguridad-1-1-1024x512.jpg',
+//     imageWidth: 400,
+//     imageHeight: 200,
+//     imageAlt: 'Custom image',
+//   })
 
-}
+// }
 
-export default ItemListContainer
+// export default ItemListContainer
